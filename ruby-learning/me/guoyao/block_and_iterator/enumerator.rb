50a2387a3fb04d2a72debc1d93cc7b4ecfbe443a
guoyao <guoyao.dev@gmail.com>
2014-02-24 15:18:38 +0800

    small changes

--- ruby-learning/me/guoyao/block_and_iterator/enumerator.rb
+++ ruby-learning/me/guoyao/block_and_iterator/enumerator.rb
@@ -59,3 +59,14 @@ p m3_palindrome.first(10)
 multiple_of_three = -> n { (n % 3).zero? }
 palindrome = -> n { n = n.to_s; n == n.reverse}
 p Integer.all.select(&multiple_of_three).select(&palindrome).first(10)
+
+def Fixnum.all
+  start = 0
+  Enumerator.new do |yielder|
+    loop do
+      yielder.yield (start += 1)
+    end
+  end
+end
+
+p Fixnum.all.first(5)
